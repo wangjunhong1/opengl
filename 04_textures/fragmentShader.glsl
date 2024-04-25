cd0673caa4db57bcd034f7aee4dbc28db67f2f88
wangjunhong <wangjunhong_@outlook.com>
2024-04-25 14:29:20 +0800

    04纹理：把得到的纹理颜色与顶点颜色混合

--- 04_textures/fragmentShader.glsl
+++ 04_textures/fragmentShader.glsl
@@ -9,5 +9,7 @@ uniform sampler2D ourTexture;// 纹理采样器
 void main()
 {
     // texture是GLSL内建的函数，用来采样纹理的颜色，它第一个参数是纹理采样器，第二个参数是对应的纹理坐标。
-    FragColor=texture(ourTexture,TexCoord);
+    // FragColor=texture(ourTexture,TexCoord);
+    // 我们还可以把得到的纹理颜色与顶点颜色混合,我们只需把纹理颜色与顶点颜色在片段着色器中相乘
+    FragColor=texture(ourTexture,TexCoord)*vec4(ourColor,1.);
 }
